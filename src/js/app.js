495d3bff20749423938e37a8af583b78d4e38dbd
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-02-09 01:23:53 +0100

    Add Header, Nav and Subscribe section

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import {settings, select, classNames} from './settings.js';
+
+const app = {
+  init: function () {},
+};
+
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,8 @@
+export const select = {
+  templateOf: {
+    home: '#template-home-page',
+    menuProduct: '#template-menu-product',
+    cartProduct: '#template-cart-product',
+    bookingWidget: '#template-booking-widget',
+  },
+};
