e2a8858492d3904f209eb1f522334199b0f6a1f8
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-02-13 20:06:19 +0100

    Add search and discover functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,154 @@
-import {settings, select, classNames} from './settings.js';
+import {select, settings, classNames} from './settings.js';
+// import Product from './components/Product.js';
+// import Cart from './components/Cart.js';
+// import Booking from './components/Booking.js';
+// import Home from './components/Home.js';
 
 const app = {
-  init: function () {},
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = [];
+    const url = settings.db.url + '/' + settings.db.songs;
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        thisApp.data.songs = data;
+      })
+      .then(() => {
+        thisApp.initPages();
+      })
+      .catch((e) => {
+        console.log('Błąd danych', e);
+      });
+  },
+
+  getElements() {
+    const thisApp = this;
+    thisApp.dom = [];
+    thisApp.dom.navWrapper = document.querySelector(select.nav.navWrapper);
+    thisApp.dom.navLinks = document.querySelectorAll(select.nav.navLinks);
+    thisApp.dom.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.dom.searchResult = document.querySelector(select.pages.search.result);
+  },
+
+  renderSearchForm() {
+    const thisApp = this;
+    thisApp.songsCategories = [];
+    for (let song of thisApp.data.songs) {
+      for (let category of song.categories) {
+        if (!thisApp.songsCategories.includes(category)) {
+          thisApp.songsCategories.push(category);
+        }
+      }
+    }
+    const source = document.querySelector(select.templateOf.searchForm).innerHTML;
+    const compiledSource = Handlebars.compile(source);
+    const compiledWithData = compiledSource(thisApp.songsCategories);
+    document.querySelector(select.pages.search.form.container).innerHTML = compiledWithData;
+  },
+
+  initNav: function () {
+    const thisApp = this;
+    const startPageId = settings.nav.defaultPage;
+    document.querySelector('[href="#' + startPageId + '"]').classList.add(classNames.nav.active);
+    thisApp.activatePage(startPageId);
+
+    for (let link of thisApp.dom.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+        for (let link of thisApp.dom.navLinks) {
+          link.classList.remove(classNames.nav.active);
+        }
+        clickedElement.classList.add(classNames.nav.active);
+        let id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+      });
+    }
+  },
+
+  initPages: function () {
+    const thisApp = this;
+    thisApp.initHomepage();
+    thisApp.initSearch();
+    thisApp.initDiscover();
+  },
+
+  initHomepage: function () {
+    const thisApp = this;
+    thisApp.randerSongsList(thisApp.data.songs, select.pages.homepage.musicContainer);
+  },
+
+  initSearch: function () {
+    const thisApp = this;
+    thisApp.renderSearchForm();
+    document.querySelector(select.pages.search.form.submit).addEventListener('click', (e) => {
+      e.preventDefault();
+      const inputValue = document.querySelector(select.pages.search.form.title).value;
+      thisApp.randerSongsList(thisApp.searchSong(inputValue), select.pages.search.musicContainer);
+      if (!thisApp.searchSong(inputValue)) {
+        thisApp.dom.searchResult.innerHTML = 'No results :(';
+      } else {
+        thisApp.dom.searchResult.innerHTML = 'We have found ' + thisApp.searchSong(inputValue).length + ' songs...';
+      }
+    });
+  },
+
+  initDiscover() {
+    const thisApp = this;
+    thisApp.randerSongsList(thisApp.randomSong(), select.pages.discover.musicContainer);
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+    for (let page of thisApp.dom.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    window.location.hash = '#/' + pageId;
+  },
+
+  randerSongsList: function (data, container) {
+    const thisApp = this;
+    const source = document.querySelector(select.templateOf.songList).innerHTML;
+    const compiledSource = Handlebars.compile(source);
+    const compiledWithData = compiledSource(data);
+    document.querySelector(container).innerHTML = compiledWithData;
+    thisApp.initMusicPlayer(container);
+  },
+
+  randomSong: function () {
+    const thisApp = this;
+    const randomNumber = Math.floor(Math.random() * thisApp.data.songs.length);
+    const randomSong = [];
+    randomSong.push(thisApp.data.songs[randomNumber]);
+    return randomSong;
+  },
+
+  searchSong: function (searchInput) {
+    const thisApp = this;
+    const searchQuery = searchInput;
+    const searchResult = thisApp.data.songs.filter((song) => {
+      return song.title.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase());
+    });
+    return searchResult;
+  },
+
+  initMusicPlayer: function (selector) {
+    const thisApp = this;
+    GreenAudioPlayer.init({
+      selector: selector + ' .song__player',
+      stopOthersOnPlay: true,
+    });
+  },
+
+  init: function () {
+    const thisApp = this;
+    thisApp.initData();
+    thisApp.getElements();
+    thisApp.initNav();
+  },
 };
 
 app.init();
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,8 +1,58 @@
 export const select = {
   templateOf: {
-    home: '#template-home-page',
-    menuProduct: '#template-menu-product',
-    cartProduct: '#template-cart-product',
-    bookingWidget: '#template-booking-widget',
+    songList: '#template-song-list',
+    searchForm: '#template-search-form',
+  },
+  containerOf: {
+    pages: '#pages',
+  },
+  nav: {
+    navWrapper: '.nav__wrapper',
+    navLinks: '.nav__wrapper a',
+  },
+  pages: {
+    homepage: {
+      container: '#homepage',
+      musicContainer: '#homepage .music .container',
+    },
+    search: {
+      container: '#search',
+      musicContainer: '#search .music .container',
+      form: {
+        container: '#search-form',
+        title: '#search-form__title',
+        submit: '#search-form__button',
+      },
+      result: '.search-result',
+    },
+    discover: {
+      container: '#discover',
+      musicContainer: '#discover .music .container',
+    },
+  },
+};
+
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+};
+
+export const settings = {
+  db: {
+    url: '//' + window.location.hostname + (window.location.hostname == 'localhost' ? ':3131' : ''),
+    songs: 'songs',
+    id: 'id',
+    title: 'title,',
+    author: 'author',
+    filename: 'filename',
+    categories: 'categories',
+    ranking: 'ranking',
+  },
+  nav: {
+    defaultPage: 'homepage',
   },
 };
